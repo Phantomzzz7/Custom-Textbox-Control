0b56957a17e27f397eb28b276d796a13cea10e06
Phantomzzz7 <raresanton14@gmail.com>
2026-04-24 02:53:53 +0300

    Finished the project. Made it possible to modify the name of each textblock.

--- Custom Textbox Control/View/UserControls/ClearableTextBox.xaml.cs
+++ Custom Textbox Control/View/UserControls/ClearableTextBox.xaml.cs
@@ -23,9 +23,30 @@ namespace Custom_Textbox_Control.View.UserControls
             InitializeComponent();
         }
 
+        private string placeHolder;
+
+        public string PlaceHolder
+        {
+            get { return placeHolder; }
+            set 
+            { 
+                placeHolder = value; 
+                tbPlaceholder.Text = placeHolder; 
+            }
+        }
+
         private void btnClear_Click(object sender, RoutedEventArgs e)
         {
+            txtInput.Clear();
+            txtInput.Focus();
+        }
 
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtInput.Text))
+                tbPlaceholder.Visibility = Visibility.Visible;
+            else
+                tbPlaceholder.Visibility = Visibility.Hidden;
         }
     }
 }
